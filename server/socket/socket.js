1b187272093ea766334dced790e7b69a6fea128c
Muhammad Umair Shad <151988665+shad-developer@users.noreply.github.com>
2025-02-25 17:17:29 +0500

    socket.js

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["https://chat-application-e1g3.onrender.com/"],
     methods: ["GET", "POST"],
   },
 });
