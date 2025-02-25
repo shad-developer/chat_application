e7ac351e3a14114639943fa8bb6c8065a6f6bc90
Muhammad Umair Shad <151988665+shad-developer@users.noreply.github.com>
2025-02-25 17:21:26 +0500

    vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://chat-application-e1g3.onrender.com/",
       },
     },
   },
