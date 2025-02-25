c953497cbcf24ca5177515c4f6cb9cf49eccc572
Muhammad Umair Shad <151988665+shad-developer@users.noreply.github.com>
2025-02-25 17:12:51 +0500

    db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log("Database Connected");
   } catch (error) {
     console.log(`failed to connect ${error}`);
