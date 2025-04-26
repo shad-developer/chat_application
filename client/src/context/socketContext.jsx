57808f29a4714d7f99b4d17956e070db672011cd
Muhammad Umair Shad <151988665+shad-developer@users.noreply.github.com>
2025-04-26 12:22:09 +0500

    socketContext.jsx

--- client/src/context/socketContext.jsx
+++ client/src/context/socketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io('https://chat-application-e1g3.onrender.com', {
+            const socket = io('https://chats-application-mern.vercel.app', {
                 query: {
                     userId: authUser._id
                 }
